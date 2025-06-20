68c2b1d831b7e5e7e9ffa565786b3a37ee58de16
amtakeuchi <amtakeuchi1@gmail.com>
2025-06-19 23:59:52 -0500

    feat: update About page with full personal content

---
+++ tina/collections/post.js
@@ -0,0 +1,54 @@
+/**
+ * @type {import('tinacms').Collection}
+ */
+export default {
+  label: "Blog Posts",
+  name: "post",
+  path: "content/post",
+  format: "mdx",
+  fields: [
+    {
+      type: "string",
+      label: "Title",
+      name: "title",
+      required: true,
+    },
+    {
+      type: "string",
+      label: "Excerpt",
+      name: "excerpt",
+      description: "A brief summary of the post",
+      ui: {
+        component: "textarea",
+      },
+    },
+    {
+      type: "datetime",
+      label: "Date",
+      name: "date",
+      required: true,
+    },
+    {
+      type: "string",
+      label: "Author",
+      name: "author",
+      defaultValue: "Adam",
+    },
+    {
+      type: "image",
+      label: "Featured Image",
+      name: "featuredImage",
+    },
+    {
+      type: "rich-text",
+      label: "Blog Post Body",
+      name: "body",
+      isBody: true,
+    },
+  ],
+  ui: {
+    router: ({ document }) => {
+      return `/posts/${document._sys.filename}`;
+    },
+  },
+}; 
