45ec8a104f2834cf6639eaa91e558b00ee99a2d6
amtakeuchi <amtakeuchi1@gmail.com>
2025-06-24 11:28:02 -0500

    Regenerate TinaCMS files with projects collection - Add updated GraphQL schema with project queries and mutations - Update TypeScript types for project collection - Update tina-lock.json with new schema - Force add generated files for Tina Cloud deployment

--- tina/__generated__/client.d.ts
+++ tina/__generated__/client.d.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ cacheDir: '/Users/adamt/Documents/GitHub/barebones-tinacms/tina/__generated__/.cache/1750782054315', url: 'http://localhost:4001/graphql', token: '2ae4faba2fc27d2516585e2cbc55913691989a22', queries,  });
+export const client = createClient({ url: 'http://localhost:4001/graphql', token: 'local', queries,  });
 export default client;
   
